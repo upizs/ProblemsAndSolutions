a1dd99e8255346096b03911eba3988ca3096b230
Toms Upis <toms.upizs@gmail.com>
2021-02-15 12:38:53 +0000

    Initial Commit

---
+++ ProblemsAndSolutions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemsAndSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new SimpleSolutions();
+            var result = solution.FillMissingNumbers(new List<int> { 1, 2, 9 });
+        }
+    }
+}
---
+++ ProblemsAndSolutions/SimpleSolutions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemsAndSolutions
+{
+    //Level1 and Level2 Problem Solutions
+    public class SimpleSolutions
+    {
+        //Fill the missing numbers
+        //Input: 1,3,5,6,7,8
+        //Output: 1,2,3,4,5,6,7,8
+        public List<int> FillMissingNumbers(List<int> numbers)
+        {
+            List<int> output = new List<int>();
+            for (var i = numbers.First(); i <= numbers.Last(); i++)
+            {
+                output.Add(i);
+            }
+            return output;
+
+            //Better practice
+            //return Enumerable.Range(numbers.First(), numbers.Last() - numbers.First() + 1).ToList();
+        }
+
+
+
+
+
+
+    }
+}
