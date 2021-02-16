0a7f9c4c6058db6a1f5a1dfdd7ef7d2d6a8c0f1b
Toms Upis <toms.upizs@gmail.com>
2021-02-16 11:11:32 +0000

    //Solved two problems was stuck on one

--- ProblemsAndSolutions/SimpleSolutions.cs
+++ ProblemsAndSolutions/SimpleSolutions.cs
@@ -24,7 +24,48 @@ namespace ProblemsAndSolutions
             //return Enumerable.Range(numbers.First(), numbers.Last() - numbers.First() + 1).ToList();
         }
 
+        //write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+        //the string should start with a 1.
+        //a string with size 6 should return :'101010'.
+        //with size 4 should return : '1010'.
+        //with size 12 should return : '101010101010'.
+        //The size will always be positive and will only use whole numbers.
 
+        public string Stringy(int size)
+        {
+
+            string output = "";
+            for (int i = 1; i < size / 2; i++)
+            {
+                output += "10";
+            }
+            
+            if (size%2 == 1)
+            {
+                output += '1';
+            }
+
+            return output;
+        }
+
+        //Given an array of integers, return a new array with each value doubled.
+        public static int[] Maps(int[] x)
+        {
+            //Map method
+            return x.Select(x => x * 2).ToArray();
+        }
+
+        //Given a non-empty array of integers, return the result of multiplying the values together in order.
+        //public int Grow(int[] x)
+        //{
+        //    var ordered = x.OrderByDescending(c => c);
+        //    int output = 1;
+        //    foreach (var number in ordered)
+        //    {
+        //        output* number;
+        //    }
+        //    return output;
+        //}
 
 
 
